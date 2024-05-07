e9ce6620d0ed66b7cacbb51103740fd414377f56
Juan-Emmanuel-Gomez-Rolon <79330849+Juan-Emmanuel-Gomez-Rolon@users.noreply.github.com>
2024-05-07 14:43:59 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import { useState, useEffect } from 'react' 
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const useImageURL = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+               const responses = await Promise.all([
+          fetch("https://jsonplaceholder.typicode.com/photos/1", { mode: "cors" }),
+          fetch("https://jsonplaceholder.typicode.com/photos/2", { mode: "cors" })
+        ]);
+
+        const data = await Promise.all(responses.map(response => {
+          if (!response.ok) {
+            throw new Error("Server error!!");
+          }
+          return response.json();
+        }));
+
+      
+        const imageURLs = data.map(response => response.url);
+
+               setImageURL(imageURLs[0]);
+
+        
+        
+          setImageURL(imageURLs[1]);
+          setLoading(false);
+        
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { imageURL, error, loading };
+};
+
 function App() {
-  const [imageURL, setImageURL] = useState (null) ;
+  const { imageURL, error, loading } = useImageURL();
 
-useEffect (() => {
-  fetch ("https:///sonplaceholder.typicode.com/photos", { mode: "cors" })
-    .then((response) => response. json())
-    .then((response) => setImageURL(response [0].url))
-    .catch((error) => console.error (error));
-}, []);
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>A network error was encountered!</p>
 
-
-return (
-  imageURL && (
-    <>
-      <h1>An image</h1>
-      <img src={imageURL} alt={"placeholder text"} />
-    </>
+  return (
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={"placeholder text"} />
+      </>
+    )
   )
-)
 }
 
-export default App
+export default App;
